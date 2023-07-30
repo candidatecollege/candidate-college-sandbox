74824e32ad7750c06a91c34ab46ee5e4202f5b64
gustonecrush <codeboxezz@gmail.com>
2023-07-30 18:34:41 +0700

    built crud on event category

--- src/app/events/categories/page.tsx
+++ src/app/events/categories/page.tsx
@@ -1,20 +1,113 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { Main, Sidebar } from '@/components'
-import { eventMenus } from '@/data/staticData'
+import { articleMenus, eventMenus } from '@/data/staticData'
 import { AddRounded, Close } from '@mui/icons-material'
 import { FormCategory } from '../components'
+import axios from 'axios'
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import { getToken } from '@/utils/token'
+import { useRouter } from 'next/navigation'
+import ListCategoryItem from '../components/ListCategoryItem'
 
 const Categories = () => {
   const [activeMenu, setActiveMenu] = useState<string>('Categories')
   const [openModalAddCategory, setOpenModalAddCategory] = useState<boolean>(false)
+  // Define state for the selected category and modal visibility separately
+  const [openModalEditCategory, setOpenModalEditCategory] = useState<boolean>(false);
+  const [selectedCategory, setSelectedCategory] = useState<any>(null);
+
+  // Open the edit modal and set the selected category when Edit button is clicked
+  const modalEditCategory = (category: any) => {
+    setSelectedCategory(category); // Set the selected category when opening the modal
+    setOpenModalEditCategory(true);
+  };
+
+  const [categories, setCategories] = useState<any[]>([])
+  const categoriesIsLoading = [1, 2, 3, 4, 5, 6]
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const router = useRouter()
+  const storedToken = getToken()
+
+  const fetchCategories = async () => {
+    setIsLoading(true)
+
+    try {
+      const response = await axios.get('https://resource.candidatecollegeind.com/api/event/categories');
+
+      setCategories(response.data.data)
+      setIsLoading(false)
+      console.log(response)
+    } catch (error) {
+      console.error(error)
+      setIsLoading(false)
+    }
+  }
+
+  const handleDeleteCategory = async (id: number) => {
+    const MySwal = withReactContent(Swal)
+
+    MySwal.fire({
+        title: 'Are you sure?',
+        text: 'You are about to delete the category. This action cannot be undone!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#1B4E6B',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!',
+    }).then(async (result: any) => {
+        if (result.isConfirmed) {
+            try {
+                const response = await axios.delete(`https://resource.candidatecollegeind.com/api/event/categories/${id}`, {
+                    headers: {
+                        Authorization: `Bearer ${storedToken}`,
+                    },
+                });
+
+                console.log(response);
+
+                // Optionally show a success message
+                MySwal.fire({
+                    title: 'Deleted!',
+                    text: 'The category has been deleted.',
+                    icon: 'success',
+                });
+
+                // Perform any other actions after successful deletion, e.g., redirecting the user, refreshing the article list, etc.
+                fetchCategories()
+
+            } catch (error) {
+                console.error(error);
+
+                // Show an error message if the delete operation fails
+                MySwal.fire({
+                    title: 'Error',
+                    text: 'Failed to delete the category. Please try again later.',
+                    icon: 'error',
+                });
+            }
+        }
+    })
+  }
+
+  const handleCategoryUploadSuccess = () => {
+    fetchCategories(); // Update the list with the new category
+    setOpenModalAddCategory(false); // Close the popup
+  };
+
+  const handleCategoryEditSuccess = () => {
+    fetchCategories(); // Update the list with the new category
+    setOpenModalEditCategory(false); // Close the popup
+  };
 
   const ModalAddCategory = () => {
     return (
         <section className='w-full h-screen absolute md:flex md:items-start md:justify-center top-0 bg-[rgba(0,0,0,0.5)]'>
             <div className="flex flex-col-gap-2 bg-white rounded-xl h-fit opacity-100 mx-5 mt-[10vh] relative md:w-[45%]">
-                <FormCategory />
+                <FormCategory onClose={handleCategoryUploadSuccess} isEdit={false} category={null} />
                 <span className='text-2xl absolute top-4 right-5 text-primary cursor-pointer' onClick={(e) => setOpenModalAddCategory(false)}>
                     <Close fontSize='inherit' color='inherit' />
                 </span>
@@ -22,6 +115,32 @@ const Categories = () => {
         </section>
     )
   }
+
+  const ModalEditCategory = (category: any) => {
+    return (
+      <section className="w-full h-screen absolute md:flex md:items-start md:justify-center top-0 bg-[rgba(0,0,0,0.5)]">
+        <div className="flex flex-col-gap-2 bg-white rounded-xl h-fit opacity-100 mx-5 mt-[10vh] relative md:w-[45%]">
+          <FormCategory onClose={handleCategoryEditSuccess} isEdit={true} category={category} />
+          <span
+            className="text-2xl absolute top-4 right-5 text-primary cursor-pointer"
+            onClick={(e) => setOpenModalEditCategory(false)} // Use the onClose function to close the modal
+          >
+            <Close fontSize="inherit" color="inherit" />
+          </span>
+        </div>
+      </section>
+    );
+  };
+
+  useEffect(() => {
+    if (!storedToken) {
+      router.push('/auth');
+    }
+  }, [storedToken, router]);
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
 
   return (
     <main className="flex w-full h-screen shadow-lg rounded-3xl bg-white text-primary">
@@ -38,15 +157,36 @@ const Categories = () => {
               }
             </div>
 
+            <ul className="flex flex-col gap-3">
+              {isLoading ? (
+                categoriesIsLoading.map((category, index) => (
+                  <ListCategoryItem category={category} key={index} handleDeleteCategory={handleDeleteCategory} isLoading={true} />
+                ))
+              ) : (
+                categories.map((category, index) => (
+                  <ListCategoryItem
+                    category={category}
+                    key={index}
+                    handleDeleteCategory={handleDeleteCategory}
+                    onEdit={modalEditCategory} // Pass the modalEditCategory function to ListCategoryItem
+                    isLoading={false}
+                  />
+                ))
+              )}
+            </ul>
+
             <div onClick={(e) => setOpenModalAddCategory(true)} className="flex items-center justify-center bg-secondary text-primary w-fit h-fit text-[3rem] p-2 font-extrabold rounded-full absolute right-10 bottom-10 cursor-pointer hover:bg-primary hover:text-white duration-700 transition-all">
                 <AddRounded color='inherit' fontSize='inherit' fontWeight={700} />
             </div>
       </Main>
       
       {/* Modal Categories */}
-      {
-        openModalAddCategory ? (<ModalAddCategory />) : (null)
-      }
+      {openModalAddCategory ? <ModalAddCategory /> : null}
+
+      {/* Pass the selected category and onClose handler to the edit modal */}
+      {openModalEditCategory ? (
+        <ModalEditCategory category={selectedCategory} />
+      ) : null}
     </main>
   )
 }
--- src/app/events/components/FormCategory.tsx
+++ src/app/events/components/FormCategory.tsx
@@ -1,23 +1,134 @@
 import React, { useState } from 'react'
 import { Input } from '@/components'
+import axios, { isAxiosError } from 'axios';
+import Swal from 'sweetalert2';
+import { getToken } from '@/utils/token';
 
-const FormCategory = () => {
-  const [name, setName] = useState<string>('')
+const FormCategory: React.FC<any> = ({ onClose, isEdit, category }) => {
+  const storedToken = getToken()
+
+  const [name, setName] = useState<string>(isEdit ? category.category.name : '')
 
   const onChangeName = (e: any) => { setName(e.target.value) }
 
-  const handleUploadCategory = (e: any) => {
+  const handleUploadCategory = async (e: any) => {
     e.preventDefault()
-    console.log(name)
+
+    const formData = new FormData()
+    formData.append('name', name)
+
+    try {
+        const response = await axios.post('https://resource.candidatecollegeind.com/api/event/categories', formData, {
+            headers: {
+                Authorization: `Bearer ${storedToken}`,
+                'Content-Type': 'mulipart/form-data',
+            },
+        })
+        console.log(response)
+        Swal.fire({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 4000,
+          timerProgressBar: true,
+          icon: 'success',
+          title: 'Successfully uploaded your new category!',
+        });
+      onClose()
+    } catch (error) {
+        console.error('Error posting data: ', error)
+        if (isAxiosError(error) && error.response && error.response.status === 401) {
+          Swal.fire({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            icon: 'error',
+            title: 'Failed to edit category, unauthenticated!',
+          })
+        } else {
+          Swal.fire({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            icon: 'error',
+            title: 'Internal server error, please edit it later!',
+          })
+        }
+        onClose()
+    }
+
+    setName('')
+  }
+
+  const handleEditCategory = async (e: any) => {
+    e.preventDefault()
+
+    const formData = new FormData()
+    formData.append('name', name)
+
+    try {
+        const response = await axios.post(`https://resource.candidatecollegeind.com/api/event/categories/${category.category.id}`, formData, {
+            headers: {
+                Authorization: `Bearer ${storedToken}`,
+                'Content-Type': 'mulipart/form-data',
+            },
+        })
+        console.log(response)
+        Swal.fire({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 4000,
+            timerProgressBar: true,
+            icon: 'success',
+            title: 'Successfully updated your category!',
+        });
+
+        onClose()
+    } catch (error) {
+        console.error('Error posting data: ', error)
+        if (isAxiosError(error) && error.response && error.response.status === 401) {
+            Swal.fire({
+              toast: true,
+              position: 'top-end',
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+              icon: 'error',
+              title: 'Failed to edit article, unauthenticated!',
+            })
+          } else {
+            Swal.fire({
+              toast: true,
+              position: 'top-end',
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+              icon: 'error',
+              title: 'Internal server error, please edit it later!',
+            })
+          }
+          onClose()
+    }
+
+    setName('')
   }
 
   return (
-    <form method="POST" encType='multipart/form-data' onSubmit={handleUploadCategory} className='flex flex-col gap-3 mt-5 w-full px-20 py-10'>
+    <form method="POST" encType='multipart/form-data' onSubmit={isEdit ? handleEditCategory : handleUploadCategory} className='flex flex-col gap-3 mt-5 w-full px-20 py-10'>
         <h1 className="font-semibold text-primary text-3xl md:text-[30px] md:w-[90%] md:leading-[90%] leading-[150%]">
-            Add Category
+            {
+                isEdit ? 'Edit' : 'Add'
+            } Category
         </h1>
         <p className="text-tersier text-sm lg:text-base md:w-[65%]">
-            Adding new category for your events
+            {
+                isEdit ? 'Edit category for your articles' : 'Adding new category for your articles'
+            }
         </p>
 
         <Input 
@@ -30,7 +141,7 @@ const FormCategory = () => {
             width='full'
         />
 
-        <button about="Submit" title="Submit" type='submit' className={`bg-secondary text-primary font-medium text-sm md:text-base rounded-full px-2 md:px-5 py-3 text-center cursor-pointer mt-6 hover:bg-primary hover:text-white md:mt-0 w-full duration-700 transition-all`}>Upload</button>
+        <button about="Submit" title="Submit" type='submit' className={`bg-secondary text-primary font-medium text-sm md:text-base rounded-full px-2 md:px-5 py-3 text-center cursor-pointer mt-6 hover:bg-primary hover:text-white md:mt-0 w-full duration-700 transition-all`}>{isEdit ? 'Edit' : 'Upload'}</button>
     </form>
   )
 }
