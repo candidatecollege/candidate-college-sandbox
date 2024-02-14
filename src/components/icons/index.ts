99aae24f7290fd6857d9568a67ee51a78b5074e6
ashafaatadhis <skullreaper387@gmail.com>
2024-02-15 02:07:11 +0700

    lupa memasukkan icons/index.ts

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,9 +1,21 @@
 import TrashIcon from "./TrashIcon";
-import PenIcon  from "./PenIcon";
+import PenIcon from "./PenIcon";
 import AddIcon from "./AddIcon";
+import DashboardIcon from "./DashboardIcon";
+import LogoutIcon from "./LogoutIcon";
+import ArticleIcon from "./ArticleIcon";
+import StickNoteIcon from "./StickyNoteIcon";
+import BriefcaseIcon from "./BriefcaseIcon";
+import PeopleIcon from "./PeopleIcon";
 
 export {
-    TrashIcon,
-    PenIcon,
-    AddIcon,
-}
+  TrashIcon,
+  PenIcon,
+  AddIcon,
+  DashboardIcon,
+  LogoutIcon,
+  ArticleIcon,
+  StickNoteIcon,
+  BriefcaseIcon,
+  PeopleIcon,
+};
